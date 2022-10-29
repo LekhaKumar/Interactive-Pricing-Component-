3b27a41e531158749bde0036b174ccb2e2866f3a
LekhaKumar <lekhahamsa613@gmail.com>
2022-10-29 15:11:40 +0800

    Adding two callback functions for checkbox and slider each

--- index.js
+++ index.js
@@ -7,88 +7,116 @@ let checkBox = document.querySelector(".form-switch #planType");
 
 
 
-checkBox.onclick = function () {
+checkBox.oninput = function () {
 
-  if(checkBox.checked == true){
-   
-    let priceInt = parseInt(price_value.textContent);
-    price_value.textContent = `${( (priceInt * .75)) * 12}`;
-    duration.textContent = "/year";
+  if (checkBox.checked == true){
+    if (slider.value == 1){
+      price_value.innerHTML = "72";
+      duration.innerHTML = "/year";
+    }else if (slider.value == 2){
+      price_value.innerHTML = "108";
+      duration.innerHTML = "/year";
+    }else if (slider.value == 3){
+      price_value.innerHTML = "144";
+      duration.innerHTML = "/year";
+    }else if (slider.value == 4){
+      price_value.innerHTML = "216";
+      duration.innerHTML = "/year";
+    }else{
+      price_value.innerHTML = "324";
+      duration.innerHTML = "/year";
+    }
+
+
+}else{
+  if (slider.value == 1){
+    price_value.innerHTML = "8";
+    duration.innerHTML = "/month";
+  }else if (slider.value == 2){
+    price_value.innerHTML = "12";
+    duration.innerHTML = "/month";
+  }else if (slider.value == 3){
+    price_value.innerHTML = "16";
+    duration.innerHTML = "/month";
+  }else if (slider.value == 4){
+    price_value.innerHTML = "24";
+    duration.innerHTML = "/month";
   }else{
-    
-    duration.textContent = "/month";
-    refreshClick();
+    price_value.innerHTML = "36";
+    duration.innerHTML = "/month";
   }
   
 }
+}
 
-function refreshClick(){
+sliderMain();
 
-  slider.oninput = function(){
-    if (slider.value == 1){
-      if (checkBox.checked==true){
+function sliderMain() {
+
+  slider.oninput = function () {
+    if (slider.value == 1) {
+      if (checkBox.checked == true) {
         price_value.innerHTML = "72";
 
-  
-      }else{
+
+      } else {
         price_value.innerHTML = "8";
 
-      } 
+      }
       pageCount.innerHTML = "10K"
     }
-    if (slider.value == 2){
-      if (checkBox.checked==true){
+    if (slider.value == 2) {
+      if (checkBox.checked == true) {
         price_value.innerHTML = "108";
 
-  
-      }else{
+
+      } else {
         price_value.innerHTML = "12";
 
-      } 
-      
+      }
+
       pageCount.innerHTML = "50K"
     }
-    if (slider.value == 3){
-      if (checkBox.checked==true){
+    if (slider.value == 3) {
+      if (checkBox.checked == true) {
         price_value.innerHTML = "144";
 
-  
-      }else{
+
+      } else {
         price_value.innerHTML = "16";
 
-      } 
-     
+      }
+
       pageCount.innerHTML = "100K"
     }
-    if (slider.value == 4){
-      if (checkBox.checked==true){
+    if (slider.value == 4) {
+      if (checkBox.checked == true) {
         price_value.innerHTML = "216";
 
-  
-      }else{
+
+      } else {
         price_value.innerHTML = "24";
 
-      } 
-     
+      }
+
       pageCount.innerHTML = "500K"
     }
-    if (slider.value == 5){
-      if (checkBox.checked==true){
+    if (slider.value == 5) {
+      if (checkBox.checked == true) {
         price_value.innerHTML = "324";
- 
 
-  
-      }else{
+
+
+      } else {
         price_value.innerHTML = "36";
 
-      } 
-      
+      }
+
       pageCount.innerHTML = "1M"
     }
   }
-  
+
 
 }
 
 
-refreshClick();
